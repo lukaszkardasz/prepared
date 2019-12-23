2bdb404920eeaa145d11694cf4429a1a8c91f85d
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-12-23 19:01:12 +0100

    prepared statement added

--- src/pl/n2god/prepared/servlet/LoginServlet.java
+++ src/pl/n2god/prepared/servlet/LoginServlet.java
@@ -11,12 +11,26 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
  * @author n2god on 23/12/2019
  * @project prepared
+ *
+ * PreparedStatement prepStmt = conn.prepareStatement("SELECT username, password FROM user WHERE username=? AND password=?;");
+ * prepStmt.setString(1, "nazwa uzytkownika");
+ * prepStmt.setString(2, "hasło");
+ * Oprócz metody setString() istnieją także odpowiedniki dla innych typów danych jak setInt(), setDouble(), setArray() o
+ * raz ogólna metoda setObject(), którą w zasadzie możemy zastąpić wszystkie inne, ponieważ może ona przyjąć obiekt dowolnego typu.
+ * W metodzie jako pierwszy argument podajemy numer parametru, który chcemy ustawić (numerując od 1) a jako drugi argument wartość,
+ * którą chcemy wstawić pod dany znak zapytania / parametr.
+ * Zabezpieczenie przed SQL Injection polega na tym, że używając np. metody setString() wstawiana wartość zawsze będzie
+ * napisem, a nie np. dalszym fragmentem zapytania. Jeżeli więc ktoś teraz spróbuje jako hasło wstawić "haslo" OR '1'='1'; -- "
+ * to całe zapytanie będzie wyglądało tak:
+ * SELECT username, password FROM user WHERE username='cokolwiek' AND password='haslo" OR \'1\'=\'1\'; -- ';1
+ * Całe wyrażenie 'haslo" OR \'1\'=\'1\'; -- ' jest tutaj traktowane jako napis a nie kawałek zapytania SQL.
  */
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -28,15 +42,18 @@ public class LoginServlet extends HttpServlet {
 			e.printStackTrace();
 			response.sendError(500);
 		}
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		//pass2" OR '1'='1'; --
-		final String sqlQuery = buildQuery(username, password);
-		System.out.println(sqlQuery);
 
+
+		final String sqlQuery = "SELECT username, password FROM user WHERE username=? AND password=?";
 		try(Connection connection = dataSource.getConnection();
-		    Statement statement = connection.createStatement();
-		    ResultSet resultSet = statement.executeQuery(sqlQuery);) {
+		    PreparedStatement statement = connection.prepareStatement(sqlQuery);) {
+
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			statement.setString(1, username);
+			statement.setString(2, password);
+			ResultSet resultSet = statement.executeQuery();
+
 			if (resultSet.next()){
 				String userFound = resultSet.getString("username");
 				request.getSession().setAttribute("username", userFound);
@@ -54,14 +71,6 @@ public class LoginServlet extends HttpServlet {
 			e.printStackTrace();
 			response.sendError(500);
 		}
-	}
-
-	private String buildQuery(String username, String password) {
-		//pass2" OR '1'='1'; --
-		return "SELECT username, password FROM user WHERE "
-		+ "username=" + "\"" + username + "\" "
-		+ "AND "
-		+ "password=" + "\"" + password + "\";";
 	}
 
 	private DataSource getDataSource() throws NamingException {
